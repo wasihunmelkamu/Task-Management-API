370c7cfc2e729b4641cf3ab3795a0049dffe77b8
Wasihun Melkamu <melkamuwasihun45@gmail.com>
2026-01-27 14:04:31 -0900

    built endpoint

--- app/api/endpoints.py
+++ app/api/endpoints.py
@@ -2,4 +2,20 @@ from fastapi import APIRouter,Depends,HTTPException
 from sqlalchemy.orm import Session
 from ..import models,schemas,database
 router=APIRouter()
+@router.post("/tasks/",response_model=schemas.Task)
+def  create_task(task:schemas.TaskCreate,db:Session=Depends(database.get_db)):
+    db_task=models.Task(**task.dict())
+    db.add(db_task)
+    db.commit()
+    db.refresh(db_task)
+    return db_task
 
+@router.get("/tasks/",response_modle=list[schemas.Task])
+def read_tasks(skip:int=0,limit:int=100,db:Session=Depends(database.get_db))
+   return db.query(models.Task).offset(skip).limit(limit).all()
+@router.get("/tasks/{task_id}", response_model=schemas.Task)
+def read_task(task_id: int, db: Session = Depends(database.get_db)):
+    task = db.query(models.Task).filter(models.Task.id == task_id).first()
+    if not task:
+        raise HTTPException(status_code=404, detail="Task not found")
+    return task
